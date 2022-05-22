6753ce0351acdfa302102e5e07f9139ef7186fbb
flingyp <1213563369@qq.com>
2022-05-22 14:18:14 +0800

    feat: fist Commit Docs

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,9 +5,9 @@
 import { defineConfig } from "vitepress";
 
 const VitePressConfig = defineConfig({
-  base: "/vitepress-basic-docs/",
-  title: "Basic Docs",
-  description: "基于 Vitepress 搭建的项目文档框架",
+  base: "/vue-admin-pro-docs/",
+  title: "Vue Admin Rro",
+  description: "基于Vue3 + TypeScript + NaiveUI等技术栈搭建的通用后台系统",
   head: [
     ["meta", { name: "author", content: "FlingYP" }],
     [
@@ -30,7 +30,7 @@ const VitePressConfig = defineConfig({
   ],
   themeConfig: {
     logo: "/docs-logo.png", // 定义顶部最左侧Logo
-    docsRepo: "flingyp/vitepress-basic-docs", // 定义顶部最右侧GitHub仓库网址
+    docsRepo: "flingyp/vue-admin-pro", // 定义顶部最右侧GitHub仓库网址
     repoLabel: "GitHub",
     // 顶部导航栏
     nav: [
@@ -42,12 +42,12 @@ const VitePressConfig = defineConfig({
         text: "相关链接",
         items: [
           {
-            text: "文档源码",
-            link: "https://github.com/flingyp/vitepress-basic-docs",
+            text: "项目源码",
+            link: "https://github.com/flingyp/vue-admin-pro",
           },
           {
             text: "预览地址",
-            link: "http://yyblog.top/vitepress-basic-docs",
+            link: "http://yyblog.top/vue-admin-pro",
           },
         ],
       },
@@ -56,15 +56,11 @@ const VitePressConfig = defineConfig({
     sidebar: {
       "/guide/": [
         {
-          text: "项目指南",
+          text: "指南",
           children: [
             {
               text: "介绍",
               link: "/guide/index",
-            },
-            {
-              text: "快速上手",
-              link: "/guide/start",
             },
           ],
         },
--- index.md
+++ index.md
@@ -1,9 +1,9 @@
 ---
 home: true
 heroImage: /docs-logo.png
-heroAlt: Docs Logo
-heroText: Basic Docs
-tagline: 一个基于 Vitepress 搭建的项目文档框架
+heroAlt: Vue Admin Pro
+heroText: Vue Admin Pro
+tagline: 基于Vue3 + TypeScript + NaiveUI等技术栈搭建的通用后台系统
 
 actionText: 快速开始 →
 actionLink: /guide/
@@ -12,12 +12,18 @@ altActionText: 在线预览
 altActionLink: http://yyblog.top/vitepress-basic-docs
 
 features:
-  - title: 😉 Vue3
-    details: 一款用于构建 Web 界面，易学易用，性能出色且功能丰富的框架。
-  - title: 😁 VitePress
-    details: VitePress 是 VuePress 小兄弟, 基于 Vite构建。Vue 驱动的静态站点生成器。
-  - title: 😛 TypeScript
-    details: TypeScript 是 JavaScript 的一个超集。
+  - title: 😛 权限管理
+    details: 集成前后端两种方式权限管理方式
+  - title: 😁 系统主题
+    details: 多种主题色，暗黑模式随意切换
+  - title: 🚀 插件集成
+    details: 多种常用插件集成
+  - title: 😉 最新技术栈
+    details: 拥抱 Vue3 + TypeScript 一系列Vue3版本的技术栈
+  - title: ⚡ 学习Vue3
+    details: 应该会是一个学习Vue3好的参考项目吧
+  - title: 👏 代码规范
+    details: ESlint + Prettier 代码规范，Git信息提交规范
 
 footer: MIT Licensed | Copyright © 2021-present FlingYP
 ---
